a108e2dfb672ae4b2766f6d97e95ba0a61a4233e
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2026-06-01 16:19:39 -0300

    chore: instalar cypress-cucumber-preprocessor e criar cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor')
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www3.bcb.gov.br',
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    setupNodeEvents(on, config) {
+    specPattern: 'cypress/e2e/**/*.feature',
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config)
+      on(
+        'file:preprocessor',
+        createBundler({ plugins: [createEsbuildPlugin(config)] })
+      )
       return config
     }
   }
